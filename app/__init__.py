63a7138723d3f182178f4dcd5e5092a7abedce7f
marko.mutavdzic <marko.mutavdzic@factoryww.com>
2021-12-05 13:18:36 +0100

    Fixes

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,3 @@
-from flask import Flask
 from flask_migrate import Migrate
 from app.models import db
 from app.routes.apartment_customer_routes import apc
@@ -6,6 +5,8 @@ from app.routes.apartment_routes import apa
 from app.routes.customer_routes import cus
 from app.routes.report_routes import rep
 from app.routes.user_routes import usr
+from flask import Flask
+from swagger.swagger_config import SWAGGERUI_BLUEPRINT, SWAGGER_URL
 
 migrate = Migrate()
 
@@ -17,6 +18,7 @@ def create_app():
     app.register_blueprint(cus)
     app.register_blueprint(apc)
     app.register_blueprint(rep)
+    app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
     app.config.from_json('../config.json')
 
     db.init_app(app)
@@ -25,6 +27,5 @@ def create_app():
     @app.route('/')
     def hello():
         return 'Zdravo, zdravo', 200
-
 
     return app
---
+++ swagger/swagger_config.py
@@ -0,0 +1,12 @@
+
+from flask_swagger_ui import get_swaggerui_blueprint
+
+SWAGGER_URL = '/swagger'
+API_URL = '/data/swagger.yaml'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Prodaja stanova"
+    }
+)
