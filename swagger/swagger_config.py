4728f2c651c4e5b11ed656f20cac8850cd4e217f
marko.mutavdzic <marko.mutavdzic@factoryww.com>
2021-12-06 22:14:31 +0100

    swagger user

--- swagger/swagger_config.py
+++ swagger/swagger_config.py
@@ -2,7 +2,7 @@
 from flask_swagger_ui import get_swaggerui_blueprint
 
 SWAGGER_URL = '/swagger'
-API_URL = '/data/swagger.yaml'
+API_URL = '/static/swagger.yaml'
 SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
     SWAGGER_URL,
     API_URL,
